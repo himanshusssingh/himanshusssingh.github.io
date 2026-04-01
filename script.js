525223fe1c886eda0cba13f7b2e4d14a15bc82bd
himanshusssingh <himanshusingh39397@gmail.com>
2026-04-01 23:41:30 +0530

    add animation

--- script.js
+++ script.js
@@ -4,3 +4,23 @@ function toggleMenu() {
   menu.classList.toggle("open");
   icon.classList.toggle("open");
 }
+
+
+document.addEventListener("DOMContentLoaded", () => {
+  const observer = new IntersectionObserver(
+    (entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add("show");
+        }
+      });
+    },
+    {
+      threshold: 0.2, // 👈 THIS FIXES MOST ISSUES
+    },
+  );
+
+  const elements = document.querySelectorAll(".hidden");
+
+  elements.forEach((el) => observer.observe(el));
+})
